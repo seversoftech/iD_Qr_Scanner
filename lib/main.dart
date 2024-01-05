0d8c577880deb7a37fd181c5ed377e90d14f4ebc
information <information@informations-iMac-Pro.local>
2024-01-05 18:12:01 +0100

    ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'constant/style.dart';
 import 'src/welcomePage.dart';
@@ -8,9 +9,12 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark));
+
     final textTheme = Theme.of(context).textTheme;
     return MaterialApp(
       title: 'ID Card Verification App',
